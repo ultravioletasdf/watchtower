fdbf9101118b6c9fec70e457737b2b6233c35703
ultraviolet <ultraviolet.asdf@proton.me>
2025-10-27 17:25:42 +0000

    Don't create feedback on comment reactions, clean up code, closes #26

--- cmd/web/handlers/reactions.go
+++ cmd/web/handlers/reactions.go
@@ -8,19 +8,19 @@ import (
 )
 
 func react(c *fiber.Ctx) error {
-	id := c.Params("id")
-	idInt, err := strconv.ParseInt(id, 10, 0)
+	id, err := c.ParamsInt("id")
 	if err != nil {
 		return c.SendStatus(400)
 	}
 
-	_type := c.Params("type")
-	typeInt, err := strconv.ParseInt(_type, 10, 0)
+	_type, err := c.ParamsInt("type")
 	if err != nil {
 		return c.SendStatus(400)
 	}
 
-	if _, err := deps.Clients.Reactions.React(c.Context(), &proto.ReactRequest{Session: c.Cookies("session"), VideoId: idInt, Type: int32(typeInt)}); err != nil {
+	targetType := c.QueryInt("target_type")
+
+	if _, err := deps.Clients.Reactions.React(c.Context(), &proto.ReactRequest{Session: c.Cookies("session"), VideoId: int64(id), Type: int32(_type), TargetType: proto.TargetType(targetType)}); err != nil {
 		return c.Status(500).SendString(err.Error())
 	}
 	return c.SendStatus(200)
@@ -32,7 +32,9 @@ func deleteReaction(c *fiber.Ctx) error {
 		return c.SendStatus(400)
 	}
 
-	if _, err := deps.Clients.Reactions.Remove(c.Context(), &proto.RemoveRequest{Session: c.Cookies("session"), VideoId: idInt}); err != nil {
+	targetType := c.QueryInt("target_type")
+
+	if _, err := deps.Clients.Reactions.Remove(c.Context(), &proto.RemoveRequest{Session: c.Cookies("session"), VideoId: idInt, TargetType: proto.TargetType(targetType)}); err != nil {
 		return c.Status(500).SendString(err.Error())
 	}
 	return c.SendStatus(200)
